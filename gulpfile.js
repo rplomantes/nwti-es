7d88d5008def0722f314efcecd31834fd2697daa
vincent <vincent@nephilaweb.com.ph>
2017-02-11 18:15:48 +0800

    Include Sass,gulp,bower

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,10 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+  var bpath = 'node_modules/bootstrap-sass/assets';
+  var jqueryPath = 'resources/assets/vendor/jquery';
+  mix.sass('app.scss')
+      .copy(jqueryPath + '/dist/jquery.min.js', 'public/js')
+      .copy(bpath + '/fonts', 'public/fonts') 
+      .copy(bpath + '/javascripts/bootstrap.min.js', 'public/js');
 });
